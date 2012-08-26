e59688159ff15c02686b1f6119cef326e4025829
Marcos Ganine <marcosganine@gmail.com>
2012-08-26 17:51:47 -0300

    New Rails 3.2.8 app without Test::Unit and ActiveRecord

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tracks2::Application.config.secret_token = '79486dab1809f606293a6ff606591244867270b566e772f1f0f937c39a620732b02f3ed4d1ef5f60f4b810f11559d52eaf525ea479f1a38a917eb407e6b93a02'
