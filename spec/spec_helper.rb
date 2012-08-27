b56bcbfe8a556828f7a1e893858e372c5b7476e7
Marcos Ganine <marcosganine@gmail.com>
2012-08-26 23:19:53 -0300

    Added Spork and configured it with Guard

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,38 +1,46 @@
-ENV["RAILS_ENV"] ||= 'test'
-require File.expand_path("../../config/environment", __FILE__)
-require 'rspec/rails'
-require 'rspec/autorun'
+require 'rubygems'
+require 'spork'
+#uncomment the following line to use spork with the debugger
+#require 'spork/ext/ruby-debug'
 
-Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+Spork.prefork do
 
-RSpec.configure do |config|
-  config.include Mongoid::Matchers
+  require 'rails/mongoid'
+  Spork.trap_class_method(Rails::Mongoid, :load_models)
+  Spork.trap_method(Rails::Application, :eager_load!)
 
-  # ## Mock Framework
-  #
-  # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
-  #
-  # config.mock_with :mocha
-  # config.mock_with :flexmock
-  # config.mock_with :rr
+  ENV["RAILS_ENV"] ||= 'test'
+  require File.expand_path("../../config/environment", __FILE__)
 
-  # If true, the base class of anonymous controllers will be inferred
-  # automatically. This will be the default behavior in future versions of
-  # rspec-rails.
-  config.infer_base_class_for_anonymous_controllers = false
+  Rails.application.railties.all { |r| r.eager_load! }
 
-  # Run specs in random order to surface order dependencies. If you find an
-  # order dependency and want to debug it, you can fix the order by providing
-  # the seed, which is printed after each run.
-  #     --seed 1234
-  config.order = "random"
+  require 'rspec/rails'
+  require 'rspec/autorun'
 
-  config.before(:suite) do
-    DatabaseCleaner.orm = "mongoid"
-    DatabaseCleaner.strategy = :truncation
+  Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+
+  RSpec.configure do |config|
+    config.include Mongoid::Matchers
+
+    config.infer_base_class_for_anonymous_controllers = false
+    config.order = "random"
+
+    config.treat_symbols_as_metadata_keys_with_true_values = true
+    config.filter_run :focus => true
+    config.run_all_when_everything_filtered = true
+
+    config.before(:suite) do
+      DatabaseCleaner.orm = "mongoid"
+      DatabaseCleaner.strategy = :truncation
+    end
+
+    config.before(:each) do
+      DatabaseCleaner.clean
+    end
   end
+end
 
-  config.before(:each) do
-    DatabaseCleaner.clean
-  end
+Spork.each_run do
+  # This code will be run each time you run your specs.
+
 end
